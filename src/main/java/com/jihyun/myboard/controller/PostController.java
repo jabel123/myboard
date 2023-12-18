a75dacd1644270ab87e3b3d5596392b234beb035
jihyun <372491@naver.com>
2023-12-18 19:34:15 +0900

    ### PR 피드백 수정

--- src/main/java/com/jihyun/myboard/controller/PostController.java
+++ src/main/java/com/jihyun/myboard/controller/PostController.java
@@ -2,7 +2,6 @@ package com.jihyun.myboard.controller;
 
 import com.jihyun.myboard.entity.Post;
 import com.jihyun.myboard.service.PostService;
-import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
