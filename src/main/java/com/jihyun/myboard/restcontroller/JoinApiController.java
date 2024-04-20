1e1d3459fbe60669a4bd277511e69753d3199662
jihyun <372491@naver.com>
2024-04-20 11:12:43 +0900

    ### 240420 + 로그인 기능 만들기 + axios에서 get방식(@RequestParam)과 post방식(@RequestBody)의 파라미터 요청 차이점 알기 + post방식에서도 get방식처럼 보낼 수 있는 방법 알기 + 가져온 정보 로컬스토리지에 담기

--- src/main/java/com/jihyun/myboard/restcontroller/JoinApiController.java
+++ src/main/java/com/jihyun/myboard/restcontroller/JoinApiController.java
@@ -1,5 +1,6 @@
 package com.jihyun.myboard.restcontroller;
 
+import com.jihyun.myboard.dto.LoginDTO;
 import com.jihyun.myboard.entity.Exercise;
 import com.jihyun.myboard.service.ExerciseService;
 import com.jihyun.myboard.service.JoinService;
@@ -29,15 +30,25 @@ public class JoinApiController {
         return usernameExist;
     }
 
+    //@RequestBody 방식으로 보내기
+//    @PostMapping("/join/login")
+//    public String selectLogin(@RequestBody LoginDTO loginDTO) {
+//        log.info(loginDTO.getUsernameIn(), loginDTO.getPasswordIn());
+//
+//        if(joinService.selectLogin(loginDTO.getUsernameIn(), loginDTO.getPasswordIn())) {
+//            return "success";
+//        } else return "fail";
+//
+//    }
+
+    // @RequestParam 방식으로 보내기
     @PostMapping("/join/login")
-    public String selectLogin(@RequestParam("usernameIn") String username,
+    public LoginDTO selectLogin(@RequestParam("usernameIn") String username,
                               @RequestParam("passwordIn") String password) {
         log.info(username, password);
+        LoginDTO loginDTO = joinService.selectLogin(username, password);
 
-        if(joinService.selectLogin(username, password)) {
-            return "success";
-        } else return "fail";
-
+        return loginDTO;
     }
 
 }
