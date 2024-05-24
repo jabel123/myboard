7e676b86e919a811678af27f2e7337cd37021ff8
jihyun <372491@naver.com>
2024-05-24 23:23:06 +0900

    ### 240524 + jwt 기능을 부가적인 관점으로 나누어 AOP 적용하기

--- src/main/java/com/jihyun/myboard/aop/RestAOP.java
+++ src/main/java/com/jihyun/myboard/aop/RestAOP.java
@@ -1,10 +1,17 @@
 package com.jihyun.myboard.aop;
 
+import com.jihyun.myboard.dto.ContentDTO;
+import com.jihyun.myboard.exception.MyException;
+import com.jihyun.myboard.token.TokenUtil;
+import io.jsonwebtoken.Claims;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 @Component
 @Aspect
@@ -17,5 +24,17 @@ public class RestAOP {
     @AfterReturning("execution(* com.jihyun.myboard.restcontroller.ContentApiController.*(..))")
     public void logAfter(JoinPoint joinPoint) {
         System.out.println("After calling method: " + joinPoint.getSignature().getName());
+    }
+
+    @Before("execution(* com.jihyun.myboard.restcontroller.ContentApiController.getContentDTO(..)) && args(contentDTO, jwt)")
+    public void checkAuthorization(@RequestBody ContentDTO contentDTO,
+                                   @RequestHeader("Authorization") String jwt) {
+        Claims claims = TokenUtil.parseJwtToken(jwt);
+        String tokenRole = (String) claims.get("role");
+        System.out.println("유저의 권한: " + tokenRole + " AOP 클래스에서 실행");
+
+        if (!tokenRole.equals("admin")) {
+            throw new MyException("admin이 아닙니다");
+        }
     }
 }
